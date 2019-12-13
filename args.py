a1c9d0e88312cb1f2c950a364ed77c5d7690ddc7
1999ADEK <b06201018@ntu.edu.tw>
2019-12-14 07:41:12 +0800

    Fix help description again

--- args.py
+++ args.py
@@ -8,7 +8,7 @@ def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--start-date', required=True, help='Crawl Start date', dest='start_date')
     parser.add_argument('--end-date', required=True, help='Crawl End date', dest='end_date')
-    parser.add_argument('--output', default='crawl.csv', help='File name to save', dest='output')
+    parser.add_argument('--output', default='crawl.csv', help='Filename to save', dest='output')
     args = parser.parse_args()
     args.start_date = datetime.strptime(args.start_date, '%Y-%m-%d')
     args.end_date = datetime.strptime(args.end_date, '%Y-%m-%d')
