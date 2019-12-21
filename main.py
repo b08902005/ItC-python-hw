b8eca2aa650e3941831d1bab40bdca707d6846f7
b08902005 <58663286+b08902005@users.noreply.github.com>
2019-12-21 10:03:44 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ if __name__ == '__main__':
     args = get_args()
     crawler = Crawler()
     content = crawler.crawl(args.start_date, args.end_date)
-    # write content to file according to spec
     df = pd.DataFrame(content, columns=['Post date', 'Title', 'Content'])
     cwd = os.getcwd()
     filename = os.path.join(cwd, args.output)
